84fc0692d4a10edeb89313c56ec5e7fa5d97a401
baloght87 <baloghtamas87@gmail.com>
2026-06-28 12:27:09 +0200

    Update void_postinstall.sh

--- void_postinstall.sh
+++ void_postinstall.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+clear
+echo "Setting up a swap file..."
+sleep 5
+dd if=/dev/zero of=/swapfile bs=1M count=8192
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+echo "/swapfile none swap sw 0 0" >> /etc/fstab
+cat /etc/fstab
+ls -l
+***
+Done. 
+***'
+sleep 5
+clear
+
 echo "Installing man stuff..."
 sleep 5
 xbps-install -Sy man-db tldr
@@ -8,6 +24,7 @@ echo '
 ***
 Done. 
 ***'
+sleep 5
 clear
 
 echo "Installing microcode..."
@@ -19,6 +36,7 @@ echo '
 ***
 Done. 
 ***'
+sleep 5
 clear
 
 echo "Adding a user..."
@@ -27,11 +45,12 @@ useradd -m balogh
 passwd balogh
 usermod -aG wheel,audio,video,network,storage balogh
 id balogh
-sleep 5
 echo '
 ***
 Done. 
 ***'
+sleep 5
+clear
 
 echo "Setting up logging..."
 sleep 5
@@ -41,31 +60,35 @@ ln -s /etc/sv/nanoklogd /var/service
 usermod -aG socklog balogh
 id balogh
 ls /var/service
-sleep 5
 echo '
 ***
 Done. 
 ***'
+sleep 5
+clear
 
 echo "Installing chrony..."
 sleep 5
 xbps-install -Sy chrony
 ln -s /etc/sv/chronyd /var/service
 ls /var/service
-sleep 5
 echo '
 ***
 Done. 
 ***'
+sleep 5
+clear
 
 echo "Setting up acpid..."
 sleep 5
 xbps-install -Sfy acpid
 ln -s /etc/sv/acpid /var/service
 ls /var/service
-sleep 5
 echo '
 ***
 Done. 
 ***'
-#######  Continue Void documentation from "Network" section #####
+sleep 5
+clear
+
+echo "#######  Continue Void documentation from 'Network' section #####"
